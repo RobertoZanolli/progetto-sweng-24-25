90a8b86dbc16b44c6aa9d21d0e8b5e5e2e6b419c
Roberto Zanolli <roberto.zanolli@laifgroup.com>
2025-05-22 15:41:32 +0200

    refactor: refactor access to db to use a different class

--- gwt-notes-maven/notes-server/src/main/java/com/google/gwt/sample/notes/server/RegisterServlet.java
+++ gwt-notes-maven/notes-server/src/main/java/com/google/gwt/sample/notes/server/RegisterServlet.java
@@ -15,6 +15,7 @@ public class RegisterServlet extends HttpServlet {
     private HTreeMap<String, String> users;
     private static final Gson gson = new Gson();
     private String dbPath = null;
+    private UserDB userDB;
 
     public RegisterServlet() {
         // Default constructor for servlet container
@@ -28,31 +29,12 @@ public class RegisterServlet extends HttpServlet {
 
     @Override
     public void init() {
-        try {
-            String pathToUse = dbPath != null ? dbPath : new java.io.File("users.db").getAbsolutePath();
-            System.out.println("[RegisterServlet] Attempting to open DB at: " + pathToUse);
-            db = DBMaker.fileDB(pathToUse).make();
-            users = (HTreeMap<String, String>) db.hashMap("users").createOrOpen();
-            if (users == null) {
-                throw new RuntimeException("Failed to initialize users map: users is null after createOrOpen() at " + pathToUse);
-            }
-            System.out.println("[RegisterServlet] DB and users map initialized successfully at: " + pathToUse);
-        } catch (Exception e) {
-            users = null;
-            db = null;
-            System.err.println("RegisterServlet init error: " + e.getClass().getName() + ": " + e.getMessage());
-            e.printStackTrace();
-            try {
-                String pathToUse = dbPath != null ? dbPath : new java.io.File("users.db").getAbsolutePath();
-                System.err.println("[RegisterServlet] Failed DB path: " + pathToUse);
-            } catch (Exception ex) {
-                System.err.println("[RegisterServlet] Could not determine absolute path for users.db: " + ex.getMessage());
-            }
-        }
+        userDB = UserDB.getInstance(dbPath);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HTreeMap<String, String> users = userDB.getUsers();
         if (users == null) {
             resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
             resp.getWriter().write("User database not initialized");
@@ -88,7 +70,7 @@ public class RegisterServlet extends HttpServlet {
         }
         String hash = hashPassword(user.getPassword());
         users.put(user.getEmail(), hash);
-        db.commit();
+        userDB.commit();
         resp.setStatus(HttpServletResponse.SC_OK);
         resp.getWriter().write("User registered");
     }
